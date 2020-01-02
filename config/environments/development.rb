98c944040ae639f8f64517add40fbb3575d29334
Hieu Phan <ptrhieu.it@gmail.com>
2020-01-02 23:54:14 +0700

    use cache in dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,9 +20,10 @@ Rails.application.configure do
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
-    config.action_controller.perform_caching = false
+    config.action_controller.perform_caching = true
 
-    config.cache_store = :null_store
+    # config.cache_store = :null_store
+    config.cache_store = :redis_cache_store, { url: "redis://localhost:6379/1" }
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
@@ -42,7 +43,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-
+  config.log_level = :warn
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
